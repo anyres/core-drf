c97ab8723be34f7ea17a860f85d3c460726fa42d
niuba <niuba.zbz@gmail.com>
2018-12-11 17:12:37 +0800

    first commit
    Signed-off-by: niuba <niuba.zbz@gmail.com>

---
+++ src/tests/MockHttpAdapter.ts
@@ -0,0 +1,57 @@
+import { IAnyresRequestOptions, IAnyresResponse, IHttpAdapter } from "@anyres/core";
+import { Observable, of as observableOf } from "rxjs";
+
+export class MockHttpAdapter implements IHttpAdapter {
+  public get(url: string, options?: IAnyresRequestOptions): Observable<IAnyresResponse> {
+    return observableOf({
+      json: () => {
+        return {
+          id: 1,
+          title: "title",
+        };
+      },
+    });
+  }
+  public post(url: string, options?: IAnyresRequestOptions): Observable<IAnyresResponse> {
+    return observableOf({
+      json: () => {
+        return {
+          id: 2,
+          ...options.body,
+        };
+      },
+    });
+  }
+  public put(url: string, options?: IAnyresRequestOptions): Observable<IAnyresResponse> {
+    return observableOf({
+      json: () => {
+        return {
+          ...options.body,
+        };
+      },
+    });
+  }
+  public delete(url: string, options?: IAnyresRequestOptions): Observable<IAnyresResponse> {
+    return observableOf({
+      json: () => {
+        return {};
+      },
+    });
+  }
+  public patch(url: string, options?: IAnyresRequestOptions): Observable<IAnyresResponse> {
+    return observableOf({
+      json: () => {
+        return {
+          ...options.body,
+        };
+      },
+    });
+  }
+}
+
+// test("mock HttpAdapter", () => {
+//   const httpAdapterStatic = new MockHttpAdapter();
+//   return httpAdapterStatic.get("").toPromise().then((data) => {
+//     expect(data.json().title).toBe("title");
+//   });
+// });
